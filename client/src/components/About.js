b2259629e6efefde3cb156104e7c78bc2a4a2335
prodionov <pavelrodionov@hotmail.com>
2018-05-21 14:59:08 +0100

    wrote short bios for me and Ghassan Reference#44

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
 import profilePhoto from "../assets/profilePhotoCropped.jpg";
+import ghassanPhoto from "../assets/ghassanProfile.JPG";
 
 export default class About extends Component {
   render() {
     return (
       <div className="manual">
         <h2>About</h2>
-        <img
-          src={profilePhoto}
-          alt="photo of Pavel"
-          width="400px"
-          align="right"
-        />
+        <img src={profilePhoto} alt="Pavel" width="400px" align="right" />
+        <a href="https://www.linkedin.com/in/pavel-rodionov-89aa5037/">
+          <h3>Pavel Rodionov</h3>
+        </a>
         <p>
-          <span>Pavel Rodionov</span>
+          <br />
+          Pavel is interested in the connections between web development and
+          data science.
+        </p>
+        <p>
+          <br />He has a bachelor degree in Maths and a Master in Applied
+          Statistics and Datamining. He has two years of professional experience
+          as a data scientist and recently participated in the web development
+          bootcamp
+          <a href="https://foundersandcoders.com/" target="_blank">
+            <span> Founders and Coders </span>
+          </a>
+          in the Middle East.
+        </p>
+        <a href="https://www.linkedin.com/in/ghassan-maslamani-4428b218/">
+          <h3>Ghassan Maslamani</h3>
+        </a>
+        <img src={ghassanPhoto} alt="Ghassan" width="400px" align="right" />
+        <p>
+          <br />
+          Ghassan is intereted in mechatronics and web development.
+        </p>
+        <p>
+          He has a bachelor degree in Mechatronics Engineering. He has
+          succesfully completed
+          <a
+            href="https://www.coursera.org/account/accomplishments/certificate/JH2BK8G79AY7"
+            target="_blank"
+          >
+            <span> Coursera course </span>
+          </a>
+          on Convolutional Neural Networks and actively participates in
+          <a href="https://www.kaggle.com/" target="_blank">
+            <span> Kaggle </span>
+          </a>
+          data science competitions. Ghassan and Pavel started their
+          collaboration at the Founders and Coders bootcamp
         </p>
       </div>
     );
